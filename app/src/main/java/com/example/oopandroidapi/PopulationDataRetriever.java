299d86b62700fed0f22cf9e66c256c25c534189b
Yanli Zhang <yanli.zhang@student.lut.fi>
2024-04-11 12:49:34 +0300

    it works

--- app/src/main/java/com/example/oopandroidapi/PopulationDataRetriever.java
+++ app/src/main/java/com/example/oopandroidapi/PopulationDataRetriever.java
@@ -3,5 +3,6 @@ package com.example.oopandroidapi;
 public class PopulationDataRetriever {
     private final String API_Key = "";
 
+
     private final String Population_API_URL = "https://api.openweathermap.org/data/2.5/weather";
 }
