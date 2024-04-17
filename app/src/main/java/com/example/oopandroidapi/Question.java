15b991c59854145bea185ce35a6f9e41577217d8
Yanli Zhang <yanli.zhang@student.lut.fi>
2024-04-17 11:26:10 +0300

    it works!

--- app/src/main/java/com/example/oopandroidapi/Question.java
+++ app/src/main/java/com/example/oopandroidapi/Question.java
@@ -14,6 +14,7 @@ public class Question {
         this.userAnswer = userAnswer;
     }
     public String getQuestionText(){
+
         return questionText;
     }
     public Boolean getUserAnswer() {
